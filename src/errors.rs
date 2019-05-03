fc16d76b8a99e5e5cc0f2a74fb6173315c2a0ba3
Andrey Kutejko <andy128k@gmail.com>
2019-05-03 20:01:19 +0200

    remove failure

--- src/errors.rs
+++ src/errors.rs
@@ -1,15 +1,32 @@
 //! Error types, which can be emited by sanitization procedure.
 
-use failure::Fail;
+use std::error::Error;
+use std::fmt;
 
 /// Sanitization error
-#[derive(Fail, Debug)]
+#[derive(Debug)]
 pub enum SanitizeError {
     /// UTF-8 decoding error
-    #[fail(display = "UTF-8 decode error {}", _0)]
-    Utf8Error(#[cause] std::string::FromUtf8Error),
+    Utf8Error(std::string::FromUtf8Error),
 
     /// Serialization error
-    #[fail(display = "Serialization error {}", _0)]
-    SerializeError(#[cause] std::io::Error),
+    SerializeError(std::io::Error),
+}
+
+impl fmt::Display for SanitizeError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            SanitizeError::Utf8Error(e) => write!(f, "UTF-8 decode error {}", e),
+            SanitizeError::SerializeError(e) => write!(f, "Serialization error {}", e),
+        }
+    }
+}
+
+impl Error for SanitizeError {
+    fn source(&self) -> Option<&(dyn Error + 'static)> {
+        match self {
+            SanitizeError::Utf8Error(e) => Some(e),
+            SanitizeError::SerializeError(e) => Some(e),
+        }
+    }
 }
